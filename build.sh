e8764bd2b99844749f977f1c33b16ea60a5f7df2
Adek Maulana <adek@techdro.id>
2017-10-30 17:03:07 +0700

    Edit typo
    Signed-off-by: Adek Maulana <adek@techdro.id>

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ echo "Setting up build";
 export VERSION="1.0";
 
 echo "Zipping";
-zip -r9 SwiftDark.zip colors.tdesktop-theme siwftdark.png;
+zip -r9 SwiftDark.zip colors.tdesktop-theme swiftdark.png;
+
+echo "Renaming"
+
 mv SwiftDark.zip SwiftDark_v${VERSION}.tdesktop-theme;
