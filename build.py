766b66c0ff4237c93eb3f8b7a54fdf232ac75d1c
Adek Maulana <adek@techdro.id>
2019-03-17 01:21:38 +0700

    update 2.0
    Change-Id: Ia8e3f219aa59c6f0114a6089acd4a38d518c5feb

---
+++ build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from os.path import dirname, realpath
+from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def zip(name, version):
+    print('Creating theme...')
+    file_list = ['colors.tdesktop-theme', 'tiled.png']
+    zipname = name + '-' + version + '.zip'
+    zip_files = ZipFile(zipname, 'w', ZIP_DEFLATED)
+    with zip_files as z:
+        for files in file_list:
+            z.write(files)
+    os.rename(zipname, name + '-' + version + '.tdesktop-theme')
+
+
+def main():
+    runpath = os.getcwd()
+    rawpath = dirname(realpath(sys.argv[0]))
+    name = 'SwiftDark'
+    version = '2.0'
+    print()
+    os.chdir(rawpath)
+    zip(name, version)
+    os.chdir(runpath)
+    print()
+    print('--- done ---')
+    print()
+
+
+if __name__ == '__main__':
+    main()
